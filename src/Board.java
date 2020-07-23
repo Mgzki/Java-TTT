254dd2d39c7c3fede2baa9cc88ecff10cda0ce3e
Max <maxgrodzki@gmail.com>
2020-07-23 15:37:05 -0400

    Stackoverflow within miniMax algo

--- src/Board.java
+++ src/Board.java
@@ -1,10 +1,10 @@
 public class Board {
-    private char[][] board; // 2D array of chars
+    private final char[][] board; // 2D array of chars
 
     //Constructs the 3 x 3 grid with enumerated empty cells for the playing board
     public Board() {
         board = new char[3][3];
-        int counter = 0;
+        int counter = 1; //Moves are from 1-9
         for (int row = 0; row < 3; row++) {
             for (int col = 0; col < 3; col++) {
                 board[row][col] = (char)(counter + 48); //converts (int)counter to associated digit as char
@@ -12,22 +12,9 @@ public class Board {
             }
         }
     }
-
-    //Constructs an N by N grid with enumerated empty cells for the playing board
-    public Board(int n) {
-        board = new char[n][n];
-        int counter = 0;
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                board[row][col] = (char)(counter + 48);
-                counter++;
-            }
-        }
-    }
-
     //Sets the players move in the selected cells
     public boolean set_Move(int[] move, char player) {
-        if (board[move[0]][move[1]] != 'X' || board[move[0]][move[1]] != 'O') {
+        if (cell_Free(move)) {
             board[move[0]][move[1]] = player;
             return true;
         } else {
@@ -35,8 +22,70 @@ public class Board {
             return false;
         }
     }
+    //Gets the current value from a position on the board
     public char get_Move(int [] move){
         return board[move[0]][move[1]];
+    }
+    //Checks if the cell is free
+    public boolean cell_Free(int[] move){
+        return (board[move[0]][move[1]] != 'X' || board[move[0]][move[1]] != 'O');
+    }
+    /*Checks if there are any available moves
+     *Used for tie checking in Tic Tac Toe
+     */
+    public boolean full_Board(){
+        int[] temp = new int[2];
+        for (int i = 0; i < 3; i++){
+            temp[0] = i;
+            for (int j = 0; j < 3; j++){
+                temp[1] = j;
+                if(cell_Free(temp))
+                    return false;
+            }
+        }return true;
+    }
+    //Displays the board state
+    public void display(){
+        // display the board
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board.length; col++) {
+                System.out.print(board[row][col]);
+                if (col < board.length - 1)
+                    System.out.print("|");
+            }
+            if (row < board.length - 1)
+                System.out.println("\n-----");
+            else
+                System.out.println();
+        }
+        System.out.println();
+    }
+    public boolean check_Row(int row, char player){
+        for (int col = 0; col < 3; col++){
+            if (board[row][col] != player)
+                return false; //cell didn't match
+        }
+        return true; // must be 3 in a row
+    }
+    public boolean check_Col(int col, char player){
+        for (int row = 0; row < 3; row++){
+            if (board[row][col] != player)
+                return false; //cell didn't match
+        }
+        return true; // must be 3 in a row
+    }
+    public boolean check_Diag(int diag, char player){
+        if (diag == 0){//Top left to bottom right
+            for (int diag1 = 0; diag1 < 3; diag1++){
+                if (board[diag1][diag1] != player)
+                    return false; //cell didn't match
+            }
+        } else {//Top right to bottom left
+            for (int diag2 = 0; diag2 < 3; diag2++) {
+                if (board[diag2][2 - diag2] != player)
+                    return false; //cell didn't match
+            }
+        } return true; // must be 3 in a row
     }
 }
 
--- src/TTT.java
+++ src/TTT.java
@@ -1,8 +1,110 @@
+import java.util.Scanner;
+
 public class TTT{
+    //Changes the turn, if True it's the human players turn, otherwise AI turn
+    public static boolean change_Turn(boolean userTurn){
+        userTurn = !userTurn;
+        return userTurn;
+    }
+    //Randomly selects who goes first
+    public static boolean goes_First(){
+        boolean userTurn = (System.currentTimeMillis() % 2) == 0;
+        return userTurn;
+    }
+    //Checks the entire row, column, and both diagonals of the last move for a winning state
+    public static boolean check_Win(Board board, Character player){
+        for (int i = 0; i < 3; i++){
+            if (board.check_Row(i, player))
+                return true;
+            else if (board.check_Col(i, player))
+                return true;
+            else if (board.check_Diag(0, player))
+                return true;
+            else if (board.check_Diag(1, player))
+                return true;
+        } return false;
+    }
+    public static int[] convert_Move(int move){
+        switch(move){
+            case 1: return (new int[] {0,0});
+            case 2: return (new int[] {0,1});
+            case 3: return (new int[] {0,2});
+            case 4: return (new int[] {1,0});
+            case 5: return (new int[] {1,1});
+            case 6: return (new int[] {1,2});
+            case 7: return (new int[] {2,0});
+            case 8: return (new int[] {2,1});
+            case 9: return (new int[] {2,2});
+        } return new int[] {0,0};
+    }
+
+    public static Board get_Move(Board board, boolean userTurn){
+        int[] move = new int[2];
+        if (userTurn){
+            System.out.println("Enter your move (1-9)");
+            Scanner input = new Scanner(System.in);
+            int i = input.nextInt();
+            move = convert_Move(i);
+            board.set_Move(move, 'X');
+        } else{ //AI move
+            System.out.println("Opponent is deciding");
+            move = bot_Move(board);
+        }
+        return board;
+    }
+    public static int[] bot_Move(Board board){
+        double bestScore = Double.NEGATIVE_INFINITY;
+        int [] move = new int[2];
+        for (int i = 0; i < 9; i++){
+            move = convert_Move(i);
+            if (board.set_Move(move,'O')){
+                double score = miniMax(board, 0, false);
+                board.set_Move(move, (char)(i+48+1));
+                if (score > bestScore)
+                    bestScore = score;
+            }
+        }
+        board.set_Move(move,'O');
+        return move;
+    }
+    public static double miniMax(Board board, int depth, boolean userTurn){
+        int[] move = new int[2];
+        if (check_Win(board, 'O')) return 10; //O wins
+        if (check_Win(board, 'X')) return -10;//X wins
+        if (board.full_Board()) return 0;//Tie
+        //Maximizing turn (AI)
+        if (userTurn){
+            double bestScore = Double.NEGATIVE_INFINITY;
+            for (int i = 0; i < 9; i++){
+                move = convert_Move(i);
+                if (board.set_Move(move, 'O')){
+                    double score = miniMax(board, depth+1, false);
+                    board.set_Move(move, (char)(i+48+1));
+                    if (score > bestScore)
+                        bestScore = score;
+                }
+            } return bestScore - depth;
+        } else {
+            double bestScore = Double.POSITIVE_INFINITY;
+            for (int i = 0; i < 9; i++){
+                move = convert_Move(i);
+                if (board.set_Move(move, 'X')){
+                    double score = miniMax(board, depth+1, true);
+                    board.set_Move(move, (char)(i+48+1));
+                    if (score > bestScore)
+                        bestScore = score;
+                }
+            } return bestScore - depth;
+        }
+    }
     public static void main(String[] args){
         Board TTT = new Board();
-        int[] move = {0,0};
-        TTT.set_Move(move, 'X');
-        System.out.println(TTT.get_Move(move));
+        TTT.display();
+        boolean userTurn = goes_First();
+        while (!check_Win(TTT,'O') && !check_Win(TTT, 'X') && !TTT.full_Board()){
+            TTT = get_Move(TTT, userTurn);
+            userTurn = change_Turn(userTurn);
+            TTT.display();
+        }
     }
 }
